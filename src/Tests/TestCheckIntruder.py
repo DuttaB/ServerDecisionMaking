771b38de8dd7e014ea1284aa89cc35fe195eae3b
DuttaB <b.dutta.ee@gmail.com>
2016-04-11 16:26:34 -0400

    fix on variables

--- src/Tests/TestCheckIntruder.py
+++ src/Tests/TestCheckIntruder.py
@@ -5,14 +5,12 @@ sys.path.append('..')
 import checkIntruder as intruderC
 
 class TestCheckIntruder(unittest.TestCase):
-    def setUp(self):
+    def test_check_intruder(self):
         building_array = [7,9,45,22,21,16,19,2,30,20]
         floor_array =     [0,2,0,1,0,3,4,5,2,1]
         room_array = [0,2,0]
         xpos_array = [41,44,46,45,47,45,42,40,42]
         ypos_array = [0,1,0,1,0,1,0,1,0]
-
-    def test_check_intruder(self):
         for building in building_array:
             for floor in floor_array:
                 for room in room_array:
