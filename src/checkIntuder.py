38954ff4a88d7dfe02a4b057474cfba1c29c86ba
DuttaB <b.dutta.ee@gmail.com>
2016-04-11 15:02:23 -0400

    Intruder Checking Component

---
+++ src/checkIntuder.py
@@ -0,0 +1,26 @@
+#This function checks if the input location has an intruder or a genuine person.
+
+import httplib
+import json
+def checkIntruder(building_id, floor_id, room_id, xpos_id, ypos_id):
+    basePath = "localhost"
+    userIds = []
+    conn = httplib.HTTPConnection(basePath, 8080)
+
+    conn.request("GET", "/api/users/")
+    res = conn.getresponse()
+    data = res.read()
+    people = json.loads(data)
+    uid = 9999; #some junk value
+
+    for person in people:
+        building = person["buildingID"]
+        oor = person["floor"]
+        om = person["room"]
+        os = person["xpos"]
+        os = person["ypos"]
+        if(building == building_id and floor == floor_id and xpos == xpos_id and ypos == ypos_id):
+            uid = person["id"]
+            
+        id == 9999
+        return True;
