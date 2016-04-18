1a43d971a620acffbacf0877916af87ab6e058d7
DuttaB <b.dutta.ee@gmail.com>
2016-04-18 19:50:28 -0400

    Putting the latest code

--- src/TestsModify/team4_modify.py
+++ src/TestsModify/team4_modify.py
@@ -231,7 +231,15 @@ def checkBuildingStatus(building_id):
     data = res.read()
     return data
 	
+# This function checks if the input location has an intruder or a
+# genuine person by checking if there exists a person in that particular
+# location.
+# Inputs are building_id, floor_id, room_id, xpos_id, ypos_id
+# It returns TRUE in case its an intruder, else it returns false.
+# It raises exception, in case of errors and returns none.
+
 def checkIntruder(building_id, floor_id, room_id, xpos_id, ypos_id):
+
     try:
         basePath = "localhost"
         userIds = []
@@ -241,18 +249,20 @@ def checkIntruder(building_id, floor_id, room_id, xpos_id, ypos_id):
         data = res.read()
         people = json.loads(data)
         uid = 9999; #some junk value
+        
         for person in people:
-            building = person["buildingID"]
+            building = person["buildingId"]
             floor = person["floor"]
             room = person["room"]
             xpos = person["xpos"]
             ypos = person["ypos"]
-            if(building == building_id and floor == floor_id and xpos == xpos_id and ypos == ypos_id):
+            if(building == building_id and floor == floor_id and room == room_id and xpos == xpos_id and ypos == ypos_id):
                 uid = person["id"]
-        if id == 9999:
-            return True;
-        else:
+
+        if uid != 9999:
             return False;
+        else:
+            return True;
     except Exception as e:
        print('Error in checkIntruder: ', e)
     return None
