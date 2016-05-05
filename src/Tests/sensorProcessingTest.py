54f8b3195c9ed84f94207f78c03e581c2073ac4a
Charudatta Chitale <charu1@vt.edu>
2016-05-05 00:18:17 -0400

    Adding few more unit tests

--- src/Tests/sensorProcessingTest.py
+++ src/Tests/sensorProcessingTest.py
@@ -43,6 +43,18 @@ class TestSensorProcessing(unittest.TestCase):
         self.assertIsNotNone(emergency)
         self.assertEquals('fire', emergency)
 
+    def test_createNewFireEventLowHeatIfExisting(self):
+        self.sensor['id'] = 'temp'
+        self.sensor['type'] = 'temperature'
+        self.sensor['newData'] = '60'
+        self.sensor['oldData'] = dummy.getSensorData('temperature',\
+                self.sensor['buildingId'], self.sensor['room'])
+        dummy.shiftHistory('smoke', '1')
+        dummy.shiftLastDataHistory('smoke','1')
+        dummy.shiftLastDataHistory('temp', '60')
+        emergency = self.process.processNewSensorData(self.sensor)
+        self.assertIsNone(emergency)
+
     def test_createNewFireHighHeat(self):
         self.sensor['id'] = 'temp'
         self.sensor['type'] = 'temperature'
@@ -52,6 +64,16 @@ class TestSensorProcessing(unittest.TestCase):
         emergency = self.process.processNewSensorData(self.sensor)
         self.assertIsNotNone(emergency)
         self.assertEquals('fire', emergency)
+
+    def test_createNewFireHighHeatIfExisting(self):
+        self.sensor['id'] = 'temp'
+        self.sensor['type'] = 'temperature'
+        self.sensor['newData'] = '101'
+        self.sensor['oldData'] = dummy.getSensorData('temperature',\
+                self.sensor['buildingId'], self.sensor['room'])
+        dummy.shiftLastDataHistory('temp', '60')
+        emergency = self.process.processNewSensorData(self.sensor)
+        self.assertIsNone(emergency)
 
     def test_createNewFireSmokeChange(self):
         self.sensor['id'] = 'smoke'
@@ -82,6 +104,17 @@ class TestSensorProcessing(unittest.TestCase):
         self.assertIsNotNone(emergency)
         self.assertEquals('gas leak', emergency)
 
+    def test_gasLeakIfExisting(self):
+        self.sensor['id'] = 'gas'
+        self.sensor['type'] = 'gas'
+        self.sensor['newData'] = '1'
+        self.sensor['oldData'] = dummy.getSensorData('gas',\
+                self.sensor['buildingId'], self.sensor['room'])
+        dummy.shiftLastDataHistory('gas', '1')
+        emergency = self.process.processNewSensorData(self.sensor)
+        self.assertIsNone(emergency)
+        
+
     def test_noGasLeak(self):
         dummy.shiftHistory('gas', '0')
         self.sensor['id'] = 'gas'
@@ -111,6 +144,17 @@ class TestSensorProcessing(unittest.TestCase):
         self.assertIsNotNone(emergency)
         self.assertEquals('water leak', emergency)
 
+    def test_waterLeakHighIfExisting(self):
+        self.sensor['id'] = 'water pressure'
+        self.sensor['type'] = 'water pressure'
+        self.sensor['newData'] = '80'
+        self.sensor['oldData'] = dummy.getSensorData('water pressure',\
+                self.sensor['buildingId'], self.sensor['room'])
+        dummy.shiftLastDataHistory('water pressure', '30')
+        emergency = self.process.processNewSensorData(self.sensor)
+        self.assertIsNone(emergency)
+        
+
     def test_waterNormal(self):
         self.sensor['id'] = 'water pressure'
         self.sensor['type'] = 'water pressure'
@@ -128,7 +172,6 @@ class TestSensorProcessing(unittest.TestCase):
         self.assertIsNone(self.process.processNewSensorData(self.sensor))
 
     def test_highWeightNoIntruder(self):
-        dummy.addUser('user1')
         self.sensor['id'] = 'weight'
         self.sensor['type'] = 'weight'
         self.sensor['newData'] = '81'
@@ -145,16 +188,6 @@ class TestSensorProcessing(unittest.TestCase):
                 self.sensor['buildingId'], self.sensor['room'])
         self.assertIsNone(self.process.processNewSensorData(self.sensor))
 
-    def test_doorOpenNoIntruder(self):
-        dummy.addUser('user1')
-        dummy.shiftHistory('weight','81')
-        self.sensor['id'] = 'door'
-        self.sensor['type'] = 'door'
-        self.sensor['newData'] = '1'
-        self.sensor['oldData'] = dummy.getSensorData('door',\
-                self.sensor['buildingId'], self.sensor['room'])
-        self.assertIsNone(self.process.processNewSensorData(self.sensor))
-
     def test_doorOpenNoIntruderChild(self):
         dummy.shiftHistory('weight','40')
         self.sensor['id'] = 'door'
@@ -164,8 +197,34 @@ class TestSensorProcessing(unittest.TestCase):
                 self.sensor['buildingId'], self.sensor['room'])
         self.assertIsNone(self.process.processNewSensorData(self.sensor))
 
-    def test_doorOpenIntruder(self):
+    def test_doorOpenNoUserIntruder(self):
+        dummy.shiftLastDataHistory('weight','0')
         dummy.shiftHistory('weight','81')
+        self.sensor['id'] = 'door'
+        self.sensor['type'] = 'door'
+        self.sensor['newData'] = '1'
+        self.sensor['oldData'] = dummy.getSensorData('door',\
+                self.sensor['buildingId'], self.sensor['room'])
+        emergency = self.process.processNewSensorData(self.sensor)
+        self.assertIsNotNone(emergency)
+        self.assertEquals('intruder', emergency)
+
+    def test_doorOpenUserPresentatDoor(self):
+        dummy.addUserInPosition('user1')
+        dummy.shiftLastDataHistory('weight','80')
+        dummy.shiftHistory('weight','181')
+        self.sensor['id'] = 'door'
+        self.sensor['type'] = 'door'
+        self.sensor['newData'] = '1'
+        self.sensor['oldData'] = dummy.getSensorData('door',\
+                self.sensor['buildingId'], self.sensor['room'])
+        self.assertIsNone(self.process.processNewSensorData(self.sensor))
+        
+
+    def test_doorOpenUserPresentIntruder(self):
+        dummy.addUser('user1')
+        dummy.shiftLastDataHistory('weight','80')
+        dummy.shiftHistory('weight','181')
         self.sensor['id'] = 'door'
         self.sensor['type'] = 'door'
         self.sensor['newData'] = '1'
